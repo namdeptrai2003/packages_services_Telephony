e93f8d3d2c0d19dd782d08239da433e5610dfaf0
Martinusbe <martinusbe@gmail.com>
2016-08-26 08:49:10 +0200

    Revert "Telephony : Move light pulse settings to CmSettings"
    This reverts commit 5c578c4369fa0e596a01537b0d8d27fbf76b3871.

--- Android.mk
+++ Android.mk
@@ -13,7 +13,6 @@ LOCAL_JAVA_LIBRARIES := telephony-common voip-common ims-common
 LOCAL_STATIC_JAVA_LIBRARIES := \
         com.android.services.telephony.common \
         guava \
-        org.cyanogenmod.platform.sdk
 
 LOCAL_SRC_FILES := $(call all-java-files-under, $(src_dirs))
 LOCAL_SRC_FILES += \
