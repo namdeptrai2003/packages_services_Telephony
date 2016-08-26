509cec85993b5c88e51bfd954b3c5a8030437df3
Martinusbe <martinusbe@gmail.com>
2016-08-26 11:04:48 +0200

    Revert "Phone: Sprint MWI Quirk: Phantom message wait indicator workaround"
    This reverts commit 8716894d9affebf418a37db3583a60a66b706e5d.
    
    Conflicts:
    	src/com/android/phone/settings/VoicemailSettingsActivity.java
    
    Change-Id: I3a83f5ca9ae86669528e7d94477806b8f8fa2d62

--- src/com/android/phone/settings/VoicemailSettingsActivity.java
+++ src/com/android/phone/settings/VoicemailSettingsActivity.java
@@ -49,8 +49,6 @@ import com.android.phone.SubscriptionInfoHelper;
 import com.android.phone.vvm.omtp.OmtpVvmCarrierConfigHelper;
 import com.android.phone.vvm.omtp.sync.OmtpVvmSourceManager;
 
-import cyanogenmod.providers.CMSettings;
-
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -203,7 +201,6 @@ public class VoicemailSettingsActivity extends PreferenceActivity
     private EditPhoneNumberPreference mSubMenuVoicemailSettings;
     private VoicemailProviderListPreference mVoicemailProviders;
     private PreferenceScreen mVoicemailSettings;
-    private SwitchPreference mMwiNotification;
     private VoicemailRingtonePreference mVoicemailNotificationRingtone;
     private CheckBoxPreference mVoicemailNotificationVibrate;
     private SwitchPreference mVoicemailVisualVoicemail;
@@ -256,22 +253,6 @@ public class VoicemailSettingsActivity extends PreferenceActivity
         mSubMenuVoicemailSettings.setDialogOnClosedListener(this);
         mSubMenuVoicemailSettings.setDialogTitle(R.string.voicemail_settings_number_label);
 
-        mMwiNotification = (SwitchPreference) findPreference(BUTTON_MWI_NOTIFICATION_KEY);
-        if (mMwiNotification != null) {
-            if (getResources().getBoolean(R.bool.sprint_mwi_quirk)) {
-                mMwiNotification.setOnPreferenceChangeListener(this);
-                int mwiNotificationEnabled = CMSettings.System.getInt(getContentResolver(),
-                        CMSettings.System.ENABLE_MWI_NOTIFICATION, 0);
-                mMwiNotification.setChecked(mwiNotificationEnabled != 0);
-            } else {
-                PreferenceScreen voicemailCategory =
-                        (PreferenceScreen) findPreference(BUTTON_VOICEMAIL_CATEGORY_KEY);
-                if (voicemailCategory != null) {
-                    voicemailCategory.removePreference(mMwiNotification);
-                }
-                mMwiNotification = null;
-            }
-        }
 
         mVoicemailProviders = (VoicemailProviderListPreference) findPreference(
                 BUTTON_VOICEMAIL_PROVIDER_KEY);
@@ -370,11 +351,6 @@ public class VoicemailSettingsActivity extends PreferenceActivity
                 mVMProviderSettingsForced = false;
                 return false;
             }
-        } else if (preference == mMwiNotification) {
-            CMSettings.System.putInt(mPhone.getContext().getContentResolver(),
-                    CMSettings.System.ENABLE_MWI_NOTIFICATION,
-                    mMwiNotification.isChecked() ? 1 : 0);
-            return true;
         }
         return false;
     }
@@ -434,6 +410,7 @@ public class VoicemailSettingsActivity extends PreferenceActivity
                 mOmtpVvmCarrierConfigHelper.startDeactivation();
             }
         }
+
         // Always let the preference setting proceed.
         return true;
     }
